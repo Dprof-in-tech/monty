328276d08d60b857ce102eb2465babd22601a443
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-12-15 23:56:33 +0000

    task 2 done

---
+++ pop.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * pop - Function that removes the top element of a stack
+ * @stack: defined stack
+ * @line_number = line of opcode
+ *
+ * Return: Always 0
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *prev;
+	(void) line_number;
+
+	prev = *stack;
+	if (prev == NULL)
+	{
+		dprintf(2, "L%u: can't pope an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	*stack = (*stack)->next;
+
+	free(prev);
+
+}
