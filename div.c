ddfe8076b20fe0858e6512162b4c460f36b03d75
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-12-18 18:18:34 +0000

    advanced task 7  - division of stack done

---
+++ div.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+
+/**
+ * divide - Function to divide members of stack
+ * @stack: defined stack
+ * @line_number: opcode line number
+ *
+ * Return: Always success 0
+ */
+
+void divide(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top, *next;
+	(void) line_number;
+
+	top = *stack;
+	next = top->next;
+
+	if (top == NULL || top->next == NULL)
+	{
+		dprintf(2, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (next == 0)
+	{
+		dprintf(2, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	next =top->next;
+	next->n /= top->n;
+
+	*stack = next;
+	next->prev = NULL;
+
+	free(top);
+}
--- get-op.c
+++ get-op.c
@@ -20,6 +20,7 @@ void (*op_fun(char *string))(stack_t **stack, unsigned int line_number)
 		{"add", add},
 		{"nop", nop},
 		{"sub", sub},
+		{"div", divide},
 		{NULL, NULL},
 	};
 	int i = 0;
